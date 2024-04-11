d2d4cb2b7ae29c10f6779431898af81552e7ebcb
Mally13 <mallymutuku13@gmail.com>
2024-04-11 21:55:28 +0300

    Pascal Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+pascal_triangle - Returns a list of integers representing the
+Pascal's triangle of n.
+"""
+
+def pascal_triangle(n):
+    if n<=0:
+        return []
+    i = 0
+    triangle = []
+    while i < n:
+        innerlist = []
+        j = 0
+        while j <= i:
+            if j == 0 or j == i:
+                innerlist.append(1)
+            else:
+                innerlist.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            j += 1
+        i += 1
+        triangle.append(innerlist)
+    return triangle
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,3 @@
+# Pascal's Triangle
+
+What is Pascal's Triangle? Pascal's triangle is the triangular array of numbers that begins with 1 on the top and with 1's running down the two sides of a triangle. Each new number lies between two numbers and below them, and its value is the sum of the two numbers above it.
