4c9cc655d48d10ad49bccfa8c7460e88a0cb58da
Mally13 <mallymutuku13@gmail.com>
2024-05-02 17:13:20 +0300

    Stats

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import sys
+
+"""
+A script that reads stdin line by line and computes the metrics
+"""
+
+
+def main():
+    """Reads lines from stdin"""
+    totalFileSize = 0
+    statusCodes = {}
+    
+    try:
+        for idx, line in enumerate(sys.stdin, start=1):
+            fileSize = int(line.split()[5])
+            statusCode = line.split()[4]
+
+            totalFileSize += fileSize
+            if statusCode in statusCodes:
+                statusCodes[statusCode] += 1
+            else:
+                statusCodes[statusCode] = 1
+            if ((idx + 1) % 10 == 0):
+                print(f'File size: {totalFileSize}')
+                sortedCodes = sorted(statusCodes.items())
+                for key, value in sortedCodes:
+                    print(f'{key}: {value}')
+    except KeyboardInterrupt:    
+        print(f'File size: {totalFileSize}')
+        sortedCodes = sorted(statusCodes.items())
+        for key, value in sortedCodes:
+            print(f'{key}: {value}')
+
+
+if __name__ == '__main__':
+    main()
