449d232ea4f81804603332438c0d0d17d8652bee
Mally13 <mallymutuku13@gmail.com>
2024-05-02 17:26:22 +0300

    Stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,39 @@
 #!/usr/bin/python3
+"""
+Log parsing
+"""
+
 import sys
 
-"""
-A script that reads stdin line by line and computes the metrics
-"""
+if __name__ == '__main__':
 
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-def main():
-    """Reads lines from stdin"""
-    totalFileSize = 0
-    statusCodes = {}
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
     try:
-        for idx, line in enumerate(sys.stdin, start=1):
-            fileSize = int(line.split()[5])
-            statusCode = line.split()[4]
-
-            totalFileSize += fileSize
-            if statusCode in statusCodes:
-                statusCodes[statusCode] += 1
-            else:
-                statusCodes[statusCode] = 1
-            if ((idx + 1) % 10 == 0):
-                print(f'File size: {totalFileSize}')
-                sortedCodes = sorted(statusCodes.items())
-                for key, value in sortedCodes:
-                    print(f'{key}: {value}')
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
     except KeyboardInterrupt:
-        print(f'File size: {totalFileSize}')
-        sortedCodes = sorted(statusCodes.items())
-        for key, value in sortedCodes:
-            print(f'{key}: {value}')
-
-
-if __name__ == '__main__':
-    main()
+        print_stats(stats, filesize)
+        raise
