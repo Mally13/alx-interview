7dd327b4ad2232dccf9cb72d4a24c5f230aa0a19
Mally13 <mallymutuku13@gmail.com>
2024-05-09 16:16:14 +0300

    Task 0: UTF8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,17 +5,25 @@ represents a valid UTF8 encoding
 """
 
 
-def validUTF8(data):
+def validUTF8(data) -> bool:
     """
-    Determines if a given data set represents a valid utf8 encoding
+    Returns True if data is a valid UTF-8 encoding, else return False
+    :param data:
+    :return:
     """
-    if not all(0 <= byte <= 127 for byte in data):
-        return False
-    byte_data = bytes(data)
-    try:
-        byte_data.decode('utf-8')
-        return True
-    except UnicodeDecodeError as e:
-        return False
-    except (TypeError, ValueError):
-        return False
+    num_bytes = 0
+    for byte in data:
+        mask = 1 << 7
+        if not num_bytes:
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+        num_bytes -= 1
+    return num_bytes == 0
