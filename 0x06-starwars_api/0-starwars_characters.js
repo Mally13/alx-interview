3ceb4b5a5894adb9bfb6c1691213616f17f44945
Mally13 <mallymutuku13@gmail.com>
2024-05-23 15:37:04 +0300

    Task 0: Star wars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,23 +5,30 @@
 
 const { argv } = process;
 const request = require('request');
-const movieURL = 'https://swapi-api.alx-tools.com/api/films';
 
 if (argv.length !== 3) {
   console.error('Usage: ./0-starwars_characters.js 3');
   process.exit(1);
 }
 
-request(`${movieURL}/${argv[2]}`, (err, response, body) => {
+const url = `https://swapi.dev/api/films/${movieId}`;
+request(url, (err, response, body) => {
   if (err) {
     console.error(err);
+    return
   }
   const filmData = JSON.parse(body);
-  filmData.characters.forEach(character => {
-    request(character, (error, res, data) => {
-      if (!error) {
-        console.log(JSON.parse(data).name);
+  if (!filmData) {
+    console.error('Invalid movie id')
+  }
+  filmData.characters.forEach(characterUrl => {
+    request(characterUrl, (characterError, characterResponse, characterBody) => {
+      if (characterError) {
+        console.error(characterError)
+        return
       }
+      const characterData = JSON.parse(characterBody);
+      console.log(characterData.name);
     });
   });
 });
